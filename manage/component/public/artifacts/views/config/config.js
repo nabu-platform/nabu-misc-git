7ea5e57475f9c8ee63bee7cb460f4dd9de805c39
Alexander Verbruggen <alex@nabu.be>
2021-10-23 10:30:18 +0200

    updated

---
+++ manage/component/public/artifacts/views/config/config.js
@@ -0,0 +1,164 @@
+Vue.view("environment-config", {
+	props: {
+		project: {
+			type: String
+		},
+		branch: {
+			type: String
+		}
+	},
+	data: function() {
+		return {
+			merge: null,
+			changedOnly: true,
+			selected: null,
+			changedParamOnly: true,
+			entrySearch: null,
+			parameterSearch: null
+		}
+	},
+	activate: function(done) {
+		this.loadMerge().then(done, done);
+	},
+	computed: {
+		visibleEntries: function() {
+			if (this.merge.entries) {
+				var self = this;
+				return this.merge.entries.filter(function(entry) {
+					var show = true;
+					if (self.changedOnly && !entry.changed) {
+						show = false;
+					}
+					if (self.entrySearch) {
+						if (entry.entryId.toLowerCase().indexOf(self.entrySearch) < 0) {
+							show = false;
+						}
+					}
+					return show;
+				});
+			}
+			else {
+				return [];
+			}
+		},
+		visibleParameters: function() {
+			var self = this;
+			if (this.selected && this.selected.parameters) {
+				return this.selected.parameters.filter(function(x) {
+					var show = true;
+					if (self.changedParamOnly && !x.changed) {
+						show = false;
+					}
+					if (x.hide) {
+						if (self.isCondition(x.hide)) {
+							show = false;
+						}
+					}
+					if (x.show) {
+						if (!self.isCondition(x.show)) {
+							show = false;
+						}
+					}
+					if (self.parameterSearch) {
+						if (x.name.toLowerCase().indexOf(self.parameterSearch) < 0) {
+							show = false;
+						}
+					}
+					return show;
+				});
+			}
+			else {
+				return [];
+			}
+		},
+		categories: function() {
+			var hasUncategorized = false;
+			var categories = [];
+			this.visibleParameters.forEach(function(x) {
+				if (x.category == null) {
+					hasUncategorized = true;
+				}
+				else if (categories.indexOf(x.category) < 0) {
+					categories.push(x.category);
+				}
+			});
+			if (hasUncategorized) {
+				categories.push("Other");
+			}
+			return categories;
+		}
+	},
+	methods: {
+		isCondition: function(condition) {
+			if (!condition) {
+				return true;
+			}
+			try {
+				var parent = this;
+				var runItInScope = function() {
+					// we want to expand the parameters into a temporary "this" scope so they can be accessed by name
+					if (parent.selected && parent.selected.parameters) {
+						var self = this;
+						parent.selected.parameters.forEach(function(x) {
+							self[x.name] = x.current;
+						});
+					}
+					var result = eval(condition);
+					return !!result;
+				}
+				return runItInScope();
+			}
+			catch (exception) {
+				console.error("Could not evaluate condition", condition, exception);
+				return false;
+			}
+		},
+		loadMerge: function() {
+			var self = this;
+			return this.$services.swagger.execute("nabu.misc.git.manage.builds.merge.get", {
+				branch: this.branch,
+				project: this.project
+			}).then(function(result) {
+				Vue.set(self, "merge", result);
+			});
+		},
+		saveMerge: function() {
+			var self = this;
+			if (self.merge) {
+				var promise = this.$services.swagger.execute("nabu.misc.git.manage.builds.merge.set", {
+					branch: this.branch,
+					project: this.project,
+					body: self.merge
+				});
+				promise.then(function() {
+					self.$emit("close");
+				});
+				self.$wait({promise:promise})
+				return promise;
+			}
+		},
+		parametersForCategory: function(category) {
+			return this.visibleParameters.filter(function(x) {
+				return (category == "Other" && x.category == null)
+					|| category == x.category;
+			});
+		},
+		paramLabel: function(parameter) {
+			if (parameter.title) {
+				return parameter.title;
+			}
+			var pretty = parameter.name.replace(/([A-Z]+)/g, " $1");
+			return pretty.substring(0, 1).toUpperCase() + pretty.substring(1);
+		},
+		values: function(parameter) {
+			var values = ""
+			if (parameter.raw) {
+				values += "<div><span class='value-key'>Current raw value:</span> " + parameter.raw + "</div>";
+			}
+			if (parameter.previous) {
+				values += "<div><span class='value-key'>Previous environment value:</span> " + parameter.previous + "</div>";
+			}
+			return values;
+		}
+	}
+});
---
+++ manage/component/public/artifacts/views/config/config.tpl
@@ -0,0 +1,51 @@
+<template id="environment-config">
+	<div class="environment-config">
+		<div class="merge-actions">
+			<button class="secondary" @click="$emit('close')">Cancel</button>
+			<button class="primary" @click="saveMerge">Build</button>
+		</div>
+		<n-form class="environment-config-content">
+			<div class="merge-entries" v-if="merge.entries">
+				<h2>Entries</h2>
+				<div class="entries-filters">
+					<n-form-text v-model="entrySearch" placeholder="Search"/>
+					<n-form-checkbox v-model="changedOnly" label="Show only changed"/>
+				</div>
+				<div class="merge-entry" v-for="entry in visibleEntries" @click="selected = entry" :class="{'selected': selected == entry }">
+					<span class="entry-id">{{entry.entryId}}</span>
+				</div>
+				<div v-if="visibleEntries.length == 0">No entries match your filter</div>
+			</div>
+			<div v-else>Nothing to merge</div>
+			<div class="merge-parameters" v-if="selected && selected.parameters">
+				<h2>Parameters<span class="tag">{{selected.entryId}}</span></h2>
+				<div class="entries-filters">
+					<n-form-text v-model="parameterSearch" placeholder="Search"/>
+					<n-form-checkbox v-model="changedParamOnly" label="Show only changed"/>
+				</div>
+				<n-collapsible :title="category" v-for="category in categories">
+					<div class="merge-parameter" v-for="parameter in parametersForCategory(category)">
+						<n-form-combo v-model="parameter.current" v-if="parameter.enumeration != null && parameter.enumeration.length > 0" :items="parameter.enumeration"
+							:label="paramLabel(parameter)" :after="parameter.description"
+							:info="values(parameter)"/>
+						<n-form-switch v-else-if="parameter.type == 'boolean'" :value="parameter.current == 'true'" @input="function(value) { parameter.current = value ? 'true' : 'false' }"
+							:label="paramLabel(parameter)" :after="parameter.description"
+							:info="values(parameter)"/>
+						<n-form-text v-else v-model="parameter.current" :label="paramLabel(parameter)"
+							:after="parameter.description"
+							:info="values(parameter)"/>
+					</div>
+				</n-collapsible>
+				<div v-if="visibleParameters.length == 0">No parameters match your filter</div>
+				<div class="merge-log" v-if="selected.log">
+					<h2>Merge Log</h2>
+					<div v-html="selected.log"></div>
+				</div>
+				<div class="merge-error" v-if="selected.errorLog">
+					<h2>Error Log</h2>
+					<pre>{{selected.errorLog}}</pre>
+				</div>
+			</div>
+		</n-form>
+	</div>
+</template>
